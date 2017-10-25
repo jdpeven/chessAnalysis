422c7eae47997ca43d98761cf2127537903fc268
Jackson Peven <jdpeven@gmail.com>
2017-10-25 13:00:50 -0700

    Began work on the chess.pgn tool

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM jdpeven/nginx-python3-pip3
 #FROM nginx
 #ENV AUTHOR=JacksonPeven
 #RUN apt-get update && apt-get install python3 -y && apt-get install python3-pip -y
-#pip install python-chess[engine,gaviota]
+RUN pip3 install python-chess[engine,gaviota]
 WORKDIR /usr/share/nginx/html
+COPY ./* /usr/share/nginx/html/
+ADD games /usr/share/nginx/html/games
 COPY homePage.html /usr/share/nginx/html/index.html
 
 CMD cd /usr/share/nginx/html ; nginx -g 'daemon off;'
+RUN python3 gameParser.py
--- gameParser.py
+++ gameParser.py
@@ -1,19 +1,20 @@
 # Right now this will take a pre-downloaded game file from chess.com and 
 # iterate through to get the important information
+import chess
+import chess.pgn
 
 def parseGame(fileName):
-    f = open(fileName, "r")
-    if f.closed:
+    pgn = open(fileName)
+    if pgn.closed:
         print("Error")
     else:
         print("Success")
-    llist = f.readlines()
-    for l in llist:
-        print(l)
+    game = chess.pgn.read_game(pgn)
+    #print(game.headers["White"])
     
 
 def main():
-    name = "2389022681-PJRoastbeef_vs_jdpeven-2017_10_24.pgn"
+    name = "./games/2389022681-PJRoastbeef_vs_jdpeven-2017_10_24.pgn"
     parseGame(name)
 
 main()
