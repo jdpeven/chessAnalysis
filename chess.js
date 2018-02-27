6c3fd17a8a61199700a5b1f2d8edf1d51875f8dc
Jackson Peven <jdpeven@gmail.com>
2018-02-27 14:27:54 -0800

    Basic lookup working

--- chess.js
+++ chess.js
@@ -1,10 +1,46 @@
 
+//https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 function buttonPressed(){
-    var req = "https://api.chess.com/pub/player/" + document.getElementById("userName").value;
-    //var req = document.getElementById("userName").value;
-    document.getElementById("url").innerHTML = req;
+    var url = "https://api.chess.com/pub/player/" + document.getElementById("userNameInput").value;
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function(data){
+            if(data["code"] == 0)
+            {
+                //alert("error");
+                updateUserInfo(null);
+            }
+            else
+            {
+                console.log(data);
+                updateUserInfo(data);
+            }
+        })
+        .catch(function(error) {
+            alert(error);
+            console.log(error);
+        })
     //alert("hello");
 };
+
+function updateUserInfo(data)
+{
+    if(data == null)
+    {
+        document.getElementById("url").innerHTML = "https://api.chess.com/pub/player/";
+        document.getElementById("name").innerHTML = "User Name: ";
+        document.getElementById("followers").innerHTML = "Followers: ";
+        document.getElementById("location").innerHTML = "Location: ";
+    }
+    else 
+    {
+        document.getElementById("url").innerHTML = "https://api.chess.com/pub/player/" + document.getElementById("userNameInput").value;
+        document.getElementById("name").innerHTML = "User Name: " + data["username"];
+        document.getElementById("followers").innerHTML = "Followers: " + data["followers"];
+        document.getElementById("location").innerHTML = "Location: " + data["location"];
+    }
+
+}
 
 document.getElementById("myButton").onclick = function(){
     buttonPressed();
